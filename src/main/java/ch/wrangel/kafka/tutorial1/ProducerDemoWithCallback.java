08925d13b7bd5597ef9e88035c3062ecc149fbef
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2020-08-26 11:07:30 +0200

    Start with Producer Callback

--- src/main/java/ch/wrangel/kafka/tutorial1/ProducerDemoWithCallback.java
+++ src/main/java/ch/wrangel/kafka/tutorial1/ProducerDemoWithCallback.java
@@ -7,7 +7,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
-public class ProducerDemo {
+public class ProducerDemoWithCallback {
 
     public static void main(String[] args) {
         String bootstrapServers = "localhost:9092";
